77b39d0460f5fee4eeb754d8386ae3a78b4d919e
Habib Oumar Bah <habiboumar.bah@monboreal.ca>
2024-10-25 22:45:46 -0400

    ajout de la classe carte credit

--- Paiement/Paiement/Program.cs
+++ Paiement/Paiement/Program.cs
@@ -1,7 +1,20 @@
-﻿internal class Program
+﻿using Paiement;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        CarteCredit carteCredit1 = new CarteCredit(52256, 10000, "virement");
+        CarteCredit carteCredit2 = new CarteCredit(9466, 441, "Investisement");
+        CarteCredit carteCredit3 = new CarteCredit(86313, 4550, "achat");
+
+
+        List<CarteCredit> CarteCredits = new List<CarteCredit> { carteCredit1, carteCredit2, carteCredit3 };
+        Console.WriteLine("les cartes de credits sont :");
+        foreach (var CarteCredit in CarteCredits)
+        {
+            CarteCredit.AfficherDetails();
+        }
+
     }
 }
